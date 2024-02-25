d459755b7e9e26b9ef8397e48e55992f68364f94
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2024-02-24 20:51:53 -0500

    scss styles configuration added

--- src/home-root-config.ts
+++ src/home-root-config.ts
@@ -4,7 +4,28 @@ import {
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
+import './style.scss';
 import microfrontendLayout from "./microfrontend-layout.html";
+import singleSpaCss from "single-spa-css";
+
+const cssLifecycles = singleSpaCss({
+  cssUrls: [],
+  webpackExtractedCss: true,
+  shouldUnmount: false,
+  timeout: 500,
+  createLink(url) {
+    const linkEl = document.createElement('link');
+    linkEl.rel = 'stylesheet';
+    linkEl.href = url;
+    return linkEl;
+  },
+});
+
+cssLifecycles.mount({
+  name: 'core-styles',
+  singleSpa: null,
+  mountParcel: () => null,
+});
 
 const routes = constructRoutes(microfrontendLayout);
 const applications = constructApplications({
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ExposeRuntimeCssAssetsPlugin = require('single-spa-css/ExposeRuntimeCssAssetsPlugin.cjs');
+
+const stylesHeadler = MiniCssExtractPlugin.loader;
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "home";
@@ -23,6 +27,33 @@ module.exports = (webpackConfigEnv, argv) => {
           orgName,
         },
       }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      }),
+      new ExposeRuntimeCssAssetsPlugin({
+        filename: '[name].css',
+      }),
     ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [stylesHeadler, 'css-loader'],
+        },
+        {
+          test: /\.(css|sass|scss)$/i,
+          use: [
+            stylesHeadler,
+            'css-loader',
+            'resolve-url-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          type: 'asset',
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        },
+      ],
+    },
   });
 };
